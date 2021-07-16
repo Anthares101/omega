37703f5b172cb9bb60d59e62db4d6155df18d2ea
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2021-07-16 11:25:16 +0200

    Add version flag (#1)

--- services/ParametersParserService.py
+++ services/ParametersParserService.py
@@ -1,14 +1,16 @@
 import argparse
+from config import VERSION
 
 
 class ParametersParserService:
 	def __init__(self):
 		self.parser = argparse.ArgumentParser(description='Provides a reverse shell to a Wordpress host (You need an admin credentials!)')
-		self.parser.add_argument('-u', '--wp-url', type=str, help='The target Wordpress url', required=True)
+		self.parser.add_argument('-v', '--version', action='version', version='%(prog)s version ' + VERSION)
+		self.parser.add_argument('-u', '--wp-url', type=str, help='the target Wordpress url', required=True)
 		self.parser.add_argument('-l', '--username', type=str, help='Wordpress admin user to use for login', required=True)
 		self.parser.add_argument('-p', '--password', type=str, help='Wordpress admin password to use for login', required=True)
-		self.parser.add_argument('-H', '--lhost', type=str, help='The ip where the reverse shell should connect to', required=True)
-		self.parser.add_argument('-P', '--lport', type=int, help='The port used to listen for the reverse shell (Default: 8080)', default=8080)
+		self.parser.add_argument('-H', '--lhost', type=str, help='the ip where the reverse shell should connect to', required=True)
+		self.parser.add_argument('-P', '--lport', type=int, help='the port used to listen for the reverse shell (Default: 8080)', default=8080)
 
 	def parse_params(self) -> argparse.Namespace:
 		return self.parser.parse_args()
