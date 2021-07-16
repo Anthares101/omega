3c99584cc5b676af61ccff9b0427d1a68fa2339f
Ángel <33127189+anthares101@users.noreply.github.com>
2021-07-16 15:19:24 +0200

    Add parameters parser service tests

---
+++ tests/test_parameters_parser_service.py
@@ -0,0 +1,52 @@
+from argparse import Namespace
+import unittest, sys
+from io import StringIO
+from services import ParametersParserService
+
+
+class ParametersParserServiceTest(unittest.TestCase):
+    def test_no_arguments_parse(self):
+        sys.argv = ['./omega.py']
+        parameterParserService = ParametersParserService()
+
+        with unittest.mock.patch('sys.stderr', new=StringIO()) as fakeOutput:
+            try:
+                parameterParserService.parse_params()
+            except SystemExit:
+                self.assertTrue('error: the following arguments are required: -u/--wp-url, -l/--username, -p/--password, -H/--lhost' in fakeOutput.getvalue().strip())
+    
+    def test_arguments_parse(self):
+        sys.argv = ['./omega.py', '-u', 'http://example', '-l', 'user', '-p', 'password', '--lhost', '127.0.0.1', '--lport', '8000']
+        parameterParserService = ParametersParserService()
+
+        with unittest.mock.patch('sys.stderr', new=StringIO()):
+            try:
+                params = parameterParserService.parse_params()
+                self.assertEqual(params, Namespace(lhost='127.0.0.1', lport=8000, password='password', username='user', wp_url='http://example'))
+            except SystemExit:
+                raise Exception('Wrong parameters!')
+    
+    def test_arguments_parse_default_port(self):
+        sys.argv = ['./omega.py', '-u', 'http://example', '-l', 'user', '-p', 'password', '--lhost', '127.0.0.1']
+        parameterParserService = ParametersParserService()
+
+        with unittest.mock.patch('sys.stderr', new=StringIO()):
+            try:
+                params = parameterParserService.parse_params()
+                self.assertEqual(params, Namespace(lhost='127.0.0.1', lport=8080, password='password', username='user', wp_url='http://example'))
+            except SystemExit:
+                raise Exception('Wrong parameters!')
+    
+    def test_arguments_parse_error(self):
+        sys.argv = ['./omega.py', '-u', 'http://example', '-l', 'user', '-p', 'password', '--lhost']
+        parameterParserService = ParametersParserService()
+
+        with unittest.mock.patch('sys.stderr', new=StringIO()) as fakeOutput:
+            try:
+                parameterParserService.parse_params()
+            except SystemExit:
+                self.assertTrue('error: argument -H/--lhost: expected one argument' in fakeOutput.getvalue().strip())
+
+
+if __name__ == '__main__':
+    unittest.main()
